c77174321d8373688cde574af458a500515c1d44
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-06-13 23:59:15 +0100

    update from June 13

--- gmailUI-react-project/src/components/Main.jsx
+++ gmailUI-react-project/src/components/Main.jsx
@@ -22,7 +22,7 @@ function Main() {
                         </svg>
                     </div>
                     <div className='right'>
-                        <small>1-50 of 682https://forms.gle/9pCT1s2KFvfoHyWG7</small>
+                        <small>1-50 of 682</small>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
                         </svg>
@@ -40,7 +40,7 @@ function Main() {
                 </div>
                 <div className='mega_container'>
 
-                    <div className='section-headers'>
+                    <div className='section_headers'>
                         <div className='cont'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-inbox-fill" viewBox="0 0 16 16">
                                 <path d="M4.98 4a.5.5 0 0 0-.39.188L1.54 8H6a.5.5 0 0 1 .5.5 1.5 1.5 0 1 0 3 0A.5.5 0 0 1 10 8h4.46l-3.05-3.812A.5.5 0 0 0 11.02 4H4.98zm-1.17-.437A1.5 1.5 0 0 1 4.98 3h6.04a1.5 1.5 0 0 1 1.17.563l3.7 4.625a.5.5 0 0 1 .106.374l-.39 3.124A1.5 1.5 0 0 1 14.117 13H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .106-.374l3.7-4.625z" />
